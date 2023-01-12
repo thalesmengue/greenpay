10bb9054ad5c9c671e69ec1a113ba2acdc3d5792
thalesmengue <thalesmmachado@gmail.com>
2023-01-12 15:35:22 -0300

    feat: finish feature test

--- tests/Feature/TransactionControllerTest.php
+++ tests/Feature/TransactionControllerTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Exceptions\TransactionException;
+use App\Exceptions\UserException;
 use App\Models\User;
 use App\Models\Wallet;
+use Carbon\Carbon;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,10 +15,13 @@ use Illuminate\Foundation\Testing\WithoutEvents;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Laravel\Passport\Passport;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class TransactionControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_can_make_a_successfull_transaction(): void
     {
         $user = User::factory()->has(Wallet::factory()->state(['balance' => 200]))->create();
@@ -29,7 +35,11 @@ class TransactionControllerTest extends TestCase
             ], 200)
         ]);
 
-        $this->withoutExceptionHandling();
+        Http::fake([
+            'http://o4d9z.mocklab.io/notify' => Http::response([
+                'message' => 'Success'
+            ], 200)
+        ]);
 
         $response = $this->post(route('transaction'), [
             'payer_id' => $user->id,
@@ -37,14 +47,46 @@ class TransactionControllerTest extends TestCase
             'amount' => 100
         ]);
 
-        $response->assertStatus(201);
+        $response->assertStatus(Response::HTTP_CREATED);
         $response->assertCreated();
 
+        $transaction = $response->collect('transaction');
+
+        $this->assertDatabaseHas('wallets', [
+            'keeper_id' => $user->id,
+            'balance' => 100
+        ]);
+
+        $this->assertDatabaseHas('wallets', [
+            'keeper_id' => $receiver->id,
+            'balance' => 100
+        ]);
+
         $this->assertDatabaseHas('transactions', [
-                'payer_id' => $user->id,
-                'receiver_id' => $receiver->id,
+                'id' => $transaction['id'],
                 'amount' => 100
             ]
         );
+    }
+
+    public function test_shopkeeper_cant_send_transaction(): void
+    {
+        $user = User::factory()->has(Wallet::factory()->state(['balance' => 200]))->state(['role' => 'shopkeeper'])->create();
+        $receiver = User::factory()->has(Wallet::factory()->state(['balance' => 0]))->create();
+
+        Passport::actingAs($user);
+
+        $this->withoutExceptionHandling();
+
+        $response = $this->post(route('transaction'), [
+            'payer_id' => $user->id,
+            'receiver_id' => $receiver->id,
+            'amount' => 100,
+        ]);
+
+        $exception = UserException::cantSendTransaction();
+
+        $response->assertUnprocessable();
+        $response->assertSee($exception->getMessage());
     }
 }
